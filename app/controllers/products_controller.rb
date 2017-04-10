1967215190c587346fb8e3f5aeac40e6feff1d94
Peach Tuntiwong <peach.tuntiwong@gmail.com>
2017-04-10 13:03:13 +1000

    Display all product in tabs

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,15 @@
 class ProductsController < ApplicationController
   def app
-
+    @allPerfumes = Category.find_by(name: "Perfumes").products
+    @allBooks = Category.find_by(name: "Books").products
+    @allToys = Category.find_by(name: "Toys").products
+    @allTechnology = Category.find_by(name: "Technology").products
   end
 
-  def load
-    category = params[:category]
-    render json: Category.find_by(name: category).products
-  end
+  # def load
+  #   category = params[:category]
+  #   render json: Category.find_by(name: category).products
+  # end
 
   def create
 
