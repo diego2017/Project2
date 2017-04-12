e2d19edeb55c907b94ead2f54cdb2e5ac81f2923
Peach Tuntiwong <peach.tuntiwong@gmail.com>
2017-04-12 14:38:10 +1000

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -17,4 +17,5 @@ Rails.application.routes.draw do
   get "/signup", to: "users#new", as: "signup"
   resources :users, only: [:create]
   post "/user/new_shopping_list", to: "users#new_shopping_list", as: "new_shopping_list"
+  get "/checkout", to: "users#checkout", as: "checkout"
 end
