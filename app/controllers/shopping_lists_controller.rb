12c50167a924e5d64179dd22b2e63d3798e70a4a
Peach Tuntiwong <peach.tuntiwong@gmail.com>
2017-04-12 10:48:10 +1000

    Restructure JS

--- app/controllers/shopping_lists_controller.rb
+++ app/controllers/shopping_lists_controller.rb
@@ -3,7 +3,7 @@ class ShoppingListsController < ApplicationController
   def index
     user_id = session[:user_id]
 
-    render json: ShoppingList.where(user_id: user_id)
+    render json: ShoppingList.where(user_id: user_id).to_json(:include => :list_items)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :create]
   get "/products/:category", to: "products#load"
   resources :shopping_lists, only: [:index, :create]
-  resources :list_items, only: [:index, :show, :create]
+  resources :list_items, only: [:index, :create]
   patch "/list_items/buld_update", to: "list_items#bulk_update"
   post "/list_items/new", to: "list_items#create"
 
